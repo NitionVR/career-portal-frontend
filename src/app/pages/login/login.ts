3e6e57ac61c1f31e585410435808724ab9e4996a
NitionVR64 <amosmaganyane@gmail.com>
2025-09-28 04:48:46 +0200

    feat(auth): Implement initial login page UI

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
-export class Login {
+export class LoginPage {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
 
+  sendMagicLink() {
+    if (this.loginForm.valid) {
+      // Logic to send magic link will be implemented in a future step
+      console.log('Sending magic link to:', this.loginForm.value.email);
+    }
+  }
 }
