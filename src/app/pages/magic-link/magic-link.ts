893735ce86b8be897c8d2a007821ea94f665b185
NitionVR64 <amosmaganyane@gmail.com>
2025-09-28 04:05:05 +0200

    feat(auth): Scaffold auth service and pages (auth--001)

---
+++ src/app/pages/magic-link/magic-link.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-magic-link',
+  imports: [],
+  templateUrl: './magic-link.html',
+  styleUrl: './magic-link.css'
+})
+export class MagicLink {
+
+}
