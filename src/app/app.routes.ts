34cb1877e32a171a224c7ac07226e7ca5f24aa6c
NitionVR64 <amosmaganyane@gmail.com>
2025-09-28 04:06:46 +0200

    feat(routing): Implement authentication routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login';
+import { RegisterPage } from './pages/register/register';
+import { MagicLinkPage } from './pages/magic-link/magic-link';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPage },
+  { path: 'register', component: RegisterPage },
+  { path: 'auth/magic-link', component: MagicLinkPage },
+];
