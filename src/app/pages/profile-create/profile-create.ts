663cfaab4fe9a91bc9d357b10cc7a317a25b563e
NitionVR64 <amosmaganyane@gmail.com>
2025-09-29 21:36:37 +0200

    feat(profile): Add placeholder page and route for profile creation

---
+++ src/app/pages/profile-create/profile-create.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-create',
+  imports: [],
+  templateUrl: './profile-create.html',
+  styleUrl: './profile-create.css'
+})
+export class ProfileCreate {
+
+}
