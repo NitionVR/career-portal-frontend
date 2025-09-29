5d7468935571f7ddafe45ef1e5e673089cdd79de
NitionVR64 <amosmaganyane@gmail.com>
2025-09-29 10:11:32 +0200

    feat(api): Set up proxy for backend communication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { LucideAngularModule, Mail, ArrowRight, CheckCircle, User } from 'lucide-angular';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
     importProvidersFrom(LucideAngularModule.pick({ Mail, ArrowRight, CheckCircle, User })),
+    provideHttpClient(),
   ],
 };
