22dda41b9cbba5691aefaa21c73ff7f8bfb16b92
NitionVR64 <amosmaganyane@gmail.com>
2025-09-29 10:21:38 +0200

    fix(routing): Activate magic link route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login';
 import { RegisterPage } from './pages/register/register';
-// import { MagicLinkPage } from './pages/magic-link/magic-link';
+import { MagicLinkPage } from './pages/magic-link/magic-link';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPage },
   { path: 'register', component: RegisterPage },
-  // { path: 'auth/magic-link', component: MagicLinkPage },
+  { path: 'auth/magic-link', component: MagicLinkPage },
 ];
