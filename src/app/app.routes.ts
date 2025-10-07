43f5ec27db45440ef2df51440c9f79e6dea1dcf7
NitionVR64 <amosmaganyane@gmail.com>
2025-10-07 17:58:45 +0200

    Rename page import from ProfileCreatePage to ProfileCreate in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login';
 import { RegisterPage } from './pages/register/register';
 import { MagicLinkPage } from './pages/magic-link/magic-link';
-import { ProfileCreatePage } from './pages/profile-create/profile-create';
+import { ProfileCreate } from './pages/profile-create/profile-create';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPage },
   { path: 'register', component: RegisterPage },
   { path: 'auth/magic-link', component: MagicLinkPage },
-  { path: 'profile/create', component: ProfileCreatePage },
+  { path: 'profile/create', component: ProfileCreate },
 ];
