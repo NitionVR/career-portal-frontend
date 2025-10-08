784e83c7a22081190d475862000b08f136abed9d
NitionVR64 <amosmaganyane@gmail.com>
2025-10-08 09:01:48 +0200

    Feat: Update app.routes.ts to include homepage and set as default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginPage } from './pages/login/login';
 import { RegisterPage } from './pages/register/register';
 import { MagicLinkPage } from './pages/magic-link/magic-link';
 import { ProfileCreate } from './pages/profile-create/profile-create';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  { path: 'homepage', component: HomepageComponent },
   { path: 'login', component: LoginPage },
   { path: 'register', component: RegisterPage },
   { path: 'auth/magic-link', component: MagicLinkPage },
