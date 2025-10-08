4a684532dd7293a7ffe63fb5dc5733d37546853b
NitionVR64 <amosmaganyane@gmail.com>
2025-10-08 09:02:00 +0200

    Feat: Create sign-in.component.ts

---
+++ src/app/shared/components/sign-in/sign-in.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent {
+  @Output() closeModal = new EventEmitter<void>();
+  email: string = '';
+  rememberMe: boolean = false;
+
+  onSubmit(form: any): void {
+    if (form.valid) {
+      console.log('Magic link sent to:', this.email, 'Remember Me:', this.rememberMe);
+      // Add API call to send magic link here
+      this.closeModal.emit();
+    }
+  }
+}
