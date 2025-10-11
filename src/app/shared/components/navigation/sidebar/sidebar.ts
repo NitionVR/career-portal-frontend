8be764527dd57c0546994992e141b913595f8a14
NitionVR64 <amosmaganyane@gmail.com>
2025-10-11 22:01:31 +0200

    feat(sidebar): Implement retractable sidebar with custom tooltips.

--- src/app/shared/components/navigation/sidebar/sidebar.ts
+++ src/app/shared/components/navigation/sidebar/sidebar.ts
@@ -1,14 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, MatTooltipModule],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   @Input() isMinimized: boolean = false;
+  @Output() minimizedStateChange = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initial setup if needed
+  }
+
+  toggleMinimize(): void {
+    this.isMinimized = !this.isMinimized;
+    this.minimizedStateChange.emit(this.isMinimized);
+  }
 }
